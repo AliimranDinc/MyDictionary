0e41773911b7423395a19257864f86cab21bf990
Aliimran Dinç <gs_1905_53_@hotmail.com>
2021-01-21 20:54:15 +0300

    MyDictionary

---
+++ MyDictionary/MyDictionaryList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionaryList<TProduct, TCount>
+    {
+
+        TProduct[] Product;
+        TCount[] counts;
+
+        public MyDictionaryList()
+        {
+            Product = new TProduct[0];
+            counts = new TCount[0];
+        }
+
+        public void Add(TProduct product, TCount count)
+        {
+            TProduct[] tempKeys = Product;
+            TCount[] tempValues = counts;
+
+            Product = new TProduct[Product.Length + 1];
+            counts = new TCount[counts.Length + 1];
+
+            int i = 0;
+            while (i < tempKeys.Length)
+            {
+                Product[i] = tempKeys[i];
+                counts[i] = tempValues[i];
+                i++;
+            }
+
+            Product[Product.Length - 1] = product;
+            counts[counts.Length - 1] = count;
+
+        }
+
+        public TProduct[] Products
+        {
+            get { return Product; }
+        }
+
+        public TCount[] Counts
+        {
+            get { return counts; }
+        }
+    }
+}
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -4,9 +4,46 @@ namespace MyDictionary
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            myDictionary = new MyDictionaryList<string, string>();
+            Dictionary_Add();
+            Dictionary_List();
+        }
+
+
+        private static MyDictionaryList<string, string> myDictionary;
+
+
+        public static void Dictionary_Add()
+        {
+            myDictionary.Add("Kalem", "5");
+            myDictionary.Add("Silgi", "10");
+            myDictionary.Add("telefon", "2");
+            myDictionary.Add("Çay", "20");
+            myDictionary.Add("Bardak", "3");
+
+        }
+
+        static void Dictionary_List()
+        {
+            Console.WriteLine("Listede ki Ürünler");
+            foreach (var myDictionaryProduct in myDictionary.Products)
+            {
+                Console.WriteLine(myDictionaryProduct);
+            }
+
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("Ürünlerin Adedi");
+            foreach (var myDictionaryCount in myDictionary.Counts)
+            {
+                Console.WriteLine(myDictionaryCount);
+            }
+
+            Console.ReadLine();
         }
     }
 }
+
